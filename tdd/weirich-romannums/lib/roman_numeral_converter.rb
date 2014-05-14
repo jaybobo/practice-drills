ed88a2f7789c407d29668ab080434c940e25302a
jaybobo <info@goodproduce.net>
2014-05-14 19:21:49 -0400

    follow along Jim Weirich TDD roman numeral kata

---
+++ tdd/weirich-romannums/lib/roman_numeral_converter.rb
@@ -0,0 +1,25 @@
+class RomanNumeralConverter
+
+  DIGITS = [
+    [10, "X"],
+    [9, "IX"],
+    [5, "V"],
+    [4, "IV"],
+    [1, "I"]
+  ]
+
+
+  def convert(n)
+    result = ""
+
+    DIGITS.each do |limit, glyph|
+      while n >= limit
+        result << glyph
+        n -= limit
+      end
+    end
+
+   result
+  end
+
+end
