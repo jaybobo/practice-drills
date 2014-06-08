afbf4e24ced59ed1d23c716592dc6fa0a0b48a7d
jaybobo <info@goodproduce.net>
2014-06-07 22:02:55 -0400

    completed factorial iterative & recursive; no test

--- week1-csANDruby/factorial/source/factorial.rb
+++ week1-csANDruby/factorial/source/factorial.rb
@@ -1,7 +1,14 @@
+
 # Implement an iterative version of the factorial function
 def factorial_iterative(n)
+  (1..n-1).each {|y| n*=y }; n
 end
 
 # Implement a recursive version of the factorial function
 def factorial_recursive(n)
+  return 1 if n == 0
+  factorial_recursive(n-1) * n
 end
+
+p factorial_iterative(3)
+p factorial_recursive(3)
